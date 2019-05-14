7214a20c7dfc106df662a7baba3a0fb5afe5bd3d
Ekaterina Lagnado <ekaterinalagnado@Ekaterinas-MacBook-Pro.local>
2019-05-13 20:59:53 -0400

    my new trivia game redo files

--- javascript.js
+++ javascript.js
@@ -1,7 +1,5 @@
 
-
-
-var card = $("#quiz-area");
+var card = $("#trivia-game");
 
 var questions = [
     {
@@ -33,78 +31,63 @@ var questions = [
 ];
 
 var timer;
-
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 60,
-    unanswered: 0,
-
-
-//function d
+    counter: 10,
+// once count gets to 0 stop teh timer and display on the the html file
     countdown: function () {
         game.counter--;
         $("#counter-number").html(game.counter);
         if (game.counter === 0) {
             console.log("time up");
             game.done();
+            clearInterval(timer);
         }
     },
-// start timer function with 1 second interval
+// call the function to begin the game and set the timer 
     start: function () {
         timer = setInterval(game.countdown, 1000);
-
-        $("#sub-wrapper").prepend(
-            "<h2>Time remaining: <span id='counter-number'>20</span> Seconds</h2>"
-        );
-
+// with jquery displaying starting number 60 and removing button "start"
+        $("#sub-wrapper").prepend("<h2> Time remaining: <span id= 'counter-number'>10</span> seconds</h2>");
         $("#start").remove();
-
+// start function takes an array of quetstins defined in "question variable and allows to select by radio-button"
         for (var i = 0; i < questions.length; i++) {
             card.append("<h2>" + questions[i].question + "</h2>");
-            for (var j = 0; j < questions[i].answers.length; j++) {
-                card.append("<input type='radio' name='question-" + i +
-                    "' value='" + questions[i].answers[j] + "'>" + questions[i].answers[j]);
+            for (var x = 0; x < questions[i].answers.length; x++) {
+                card.append("<input type='radio' name='question-" + i + "' value='" + questions[i].answers[x] + "'>" + questions[i].answers[x]);
             }
         }
 
-        card.append("<button id='done'>Done</button>");
+        card.append("<button id='done'> Done</button>");
     },
-
+    // once all answers selected - done function calculates correct and incorrect answers 
     done: function () {
-        var inputs = card.append("input:checked");
-        for (var i = 0; i < inputs.lenght; i++) {
-            if (inputs[i].val() === questions[i].correctAnswer) {
+        var inputs = card.children("input:checked");
+        console.log('inside done');
+        for (var i = 0; i < inputs.length; i++) {
+            if ($(inputs[i].val() === questions[i].correctAnswer)) {
                 game.correct++;
-            } else { 
+            } else {
                 game.incorrect++;
-            
             }
-            
         }
         game.result();
     },
-    result: function() {
+// clearInterval stops the timer 
+    result: function () {
         clearInterval(timer);
-    
         $("#sub-wrapper h2").remove();
-        
-        card.html("<h2>All done!</h2>");
-        card.append("<h3> Correct Answers: " + game.correct + "</h3>");
-        card.append("<h3> Incorrect Answers: " + game.incorrect + "</h3>");
-        card.append("<h3> Unanswered: " + game.unanswered + "</h3>");
-        
+        // and resuls brougt to html page
+        card.html("<h3> Game is Over</h3>");
+        card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+        card.append("<h3>Incorrect answers: " + this.incorrect + "</h3>");
     }
 };
-
-
-
-
+// on click events to activate "strat" and "done" on the html page 
 $(document).on("click", "#start", function () {
-    game.start();
+ game.start();
 });
-
 $(document).on("click", "#done", function () {
-    game.done();
+game.done();
 });
-
